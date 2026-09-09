ef26b1df03e37a90ae0dcc0c469db6d04f02b132
yazdan-react <yazdanparast.react@gmail.com>
2026-09-09 02:59:58 -0800

    initialize survey-app-backend with Maven setup and basic Spring Boot application structure

---
+++ src/main/java/org/mapnaom/surveyappbackend/SurveyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mapnaom.surveyappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mapnaom/surveyappbackend/SurveyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mapnaom.surveyappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
